2e609fa28f6e8ab35b1cfadad22fc1707a8bb6c6
Tuan Dao <Tuan_Dao@bose.com>
2016-10-24 09:15:28 -0400

    Init commit

---
+++ backup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Copying vimrc
+cp ~/.vimrc ./
+cp ~/.bashrc ./
+cp ~/.tmux.conf ./
+cp ~/.tmux.conf.local ./
---
+++ restore.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp ./.vimrc ~/
+cp ./.bashrc ~/
+cp ./.tmux.conf ~/
+cp ./.tmux.conf.local ~/
